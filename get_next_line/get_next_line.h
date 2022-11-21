cbaf7072efeb6cebff6393698db2b39982ad0353
Luis Maria De Sousa Burnay Pereira <lde-sous@c1r1s7.42porto.com>
2022-11-21 20:03:57 +0000

    get_next_line phase 1

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lde-sous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 21:05:10 by lde-sous          #+#    #+#             */
+/*   Updated: 2022/11/21 17:13:14 by lde-sous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <strings.h>
+# include <stddef.h>
+# include <ctype.h>
+# include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 47
+# endif
+
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(char *s);
