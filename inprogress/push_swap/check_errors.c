a35ed822d86f4c85fe9888b3b701eb2960d97984
Luis Maria De Sousa Burnay Pereira <lde-sous@c1r1s7.42porto.com>
2023-03-14 14:05:50 +0000

    getting sick backup

--- inprogress/push_swap/check_errors.c
+++ inprogress/push_swap/check_errors.c
@@ -6,7 +6,7 @@
 /*   By: lde-sous <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 13:32:18 by lde-sous          #+#    #+#             */
-/*   Updated: 2023/03/03 20:47:05 by lde-sous         ###   ########.fr       */
+/*   Updated: 2023/03/14 13:49:11 by lde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 int	isbig(ssize_t num)
 {
-	if (num > 2147483647)
+	if (num > 2147483647 || num < -2147483648)
 		return (1);
-	ft_printf("ola\n");
 	return (0);
 }
 
@@ -30,27 +29,20 @@ int	check_errors(int ac, char **av)
 	i = -1;
 	ac--;
 	ptr = av;
-	while (av && ++i < ac) 
+	while (av++ && ++i < ac) 
 	{
 		j = i + 1;
-		while (j <= ac)
+		while (j <= (ac - 1))
 		{
 			if (av[0] == av[j++])
 				return (1);
 		}
-		av++;
 	}
 	i = 0;
 	av = ptr;
 	while (i < ac)
 	{
 		temp_num = ft_atoi(av[i]);
-		ft_printf("%d\n", temp_num);
-/*		if (ft_isdigit(temp_num) == 0)
-		{
-			ft_printf("khsajdh");
-			return (1);
-		}*/
 		if (isbig(temp_num) == 1)
 			return (1);
 		i++;
