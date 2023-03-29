313bb4af2071fecefcf5431d27e07d5da8eafa9a
lubuper <lubu.p@hotmail.com>
2023-03-29 10:13:45 +0100

    test

--- inprogress/push_swap/test2.c
+++ inprogress/push_swap/test2.c
@@ -6,12 +6,35 @@
 /*   By: lde-sous <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 20:12:03 by lde-sous          #+#    #+#             */
-/*   Updated: 2023/03/27 20:42:13 by lde-sous         ###   ########.fr       */
+/*   Updated: 2023/03/28 16:34:53 by lde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+
+int	*sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	if (size != 0)
+	{
+		while (i < size)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i + 1];
+				tab[i + 1] = tab[i];
+				tab[i] = temp;
+				i = 0;
+			}
+			i++;
+		}
+	}
+	return (tab);
+}
 
 int	*index_arr(char **str, int ac)
 {
@@ -22,48 +45,59 @@ int	*index_arr(char **str, int ac)
 	int	i;
 	int	j;
 
+	printf("check1");
 	arr = malloc(sizeof(ac - 1));
-	array = arr;
-	final = arr;
-	if (!arr || !array)
-		return (0);
+	array = malloc(sizeof(ac - 1));
+	final = malloc(sizeof(ac - 1));
+	if (!arr || !array || !final)
+		return (NULL);
+	printf("check2");
 	n = ac;
 	i = 0;
 	j = 0;
 	while (n > 0)
 	{
-		array[i] = ft_atoi(str[i + 1]);
+		array[i] = atoi(str[i + 1]);
 		n--;
 		i++;
 	}
-	arr = sort_int_tab(array);
+	printf("check3");
+	arr = sort_int_tab(array, (ac - 1));
+	free(array);
 	i = 0;
 	n = ac;
 	while (ac < 0)
 	{
 		j = 0;
-		while (j < n);
+		while (j < n)
 		{
 			if (array[i] == arr[j])
 				final[i] = j;
+			j++;
+		}
 		ac--;
 		i++;
 	}
-	
-	free(array);
+	printf("check4");
 	free(arr);
 	return (final);
 }
 
 
-/*while (arr[i])
-{
-:	if (find min
-}
-*/
 int	main(int ac, char **av)
 {
-	(void)ac;
-	index_arr(av, ac);
+	int	*intarray;
+	int	i;
+
+	intarray = malloc(sizeof(ac - 1));
+	if (!intarray)
+		return (0);
+	i = 0;
+	intarray = index_arr(av, (ac - 1));
+	while (i < ac)
+	{
+		printf("%d\n", intarray[i]);
+		i++;
+	}
 	return (0);
 }
