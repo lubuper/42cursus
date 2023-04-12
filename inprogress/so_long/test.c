1b4e68fd530b7a0e997f0c3c25ab1a9c2b71f491
Luis Maria De Sousa Burnay Pereira <lde-sous@c1r1s7.42porto.com>
2023-04-12 15:59:40 +0100

    solong

--- inprogress/so_long/test.c
+++ inprogress/so_long/test.c
@@ -6,7 +6,7 @@
 /*   By: lde-sous <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 15:11:35 by lde-sous          #+#    #+#             */
-/*   Updated: 2023/04/10 20:47:17 by lde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/12 15:33:45 by lde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,7 @@ unsigned char	get_r(int trgb)
 	return (((unsigned char *)&trgb)[2]);
 }
 
-unsigned char	get_g(in
-t trgb)
+unsigned char	get_g(int trgb)
 {
 	return (((unsigned char *)&trgb)[1]);
 }
@@ -66,24 +65,24 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
-/*
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
 	t_data	img;
 	int	x;
 	int	y;
 	int	colour;
 	int	i;
+	t_vars	vars;
 
 	x = 100;
 	y = 184;
 	i = 2;
 	colour = 0xccf1ff;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1024, 768, "so_long");
-	img.img = mlx_new_image(mlx, 1024, 768);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1024, 768, "so_long");
+	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+	img.img = mlx_new_image(vars.mlx, 1024, 768);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 		&img.endian);
 	while (y <= 700)
@@ -94,37 +93,6 @@ int	main(void)
 		colour *= i;
 		i++;
 	}
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
-}
-*/
-int	main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-	int	x;
-	int	y;
-	int	colour;
-	int	i;
-
-	x = 100;
-	y = 184;
-	i = 2;
-	colour = 0xccf1ff;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1024, 768, "so_long");
-	img.img = mlx_new_image(mlx, 1024, 768);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-		&img.endian);
-	while (y <= 700)
-	{
-		my_mlx_pixel_put(&img, x, y, colour);
-		my_mlx_pixel_put(&img, x + 30, y + 30, colour);
-		y++;
-		colour *= i;
-		i++;
-	}
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_loop(vars.mlx);
 }
