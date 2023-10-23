d15bf16d7c592dc1a0790f08040a2ac224cca863
Luis Pereira <lde-sous@c1r1s7.42porto.com>
2023-10-23 20:35:55 +0100

    folder name

--- inprogress/CPP01/ex02/main.cpp
+++ inprogress/CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lde-sous <lde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 18:55:31 by lde-sous          #+#    #+#             */
-/*   Updated: 2023/10/21 19:06:03 by lde-sous         ###   ########.fr       */
+/*   Updated: 2023/10/23 14:54:13 by lde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ int	main(void)
 {
 	std::string	str = "HI THIS IS BRAIN";
 	std::string	*stringPTR;
-	std::string	stringREF = *str;
+	std::string	&stringREF = str;
 //memory address of the string variable
 	std::cout << &str << std::endl;
 //memory address held by stringPTR
 	std::cout << &stringPTR << std::endl;
 //memory address held by stringREF
-	std::cout << << std::endl;
+	std::cout << &stringREF << std::endl;
 //value of string variable
-	std::cout << << std::endl;
+	std::cout << str << std::endl;
 //value pointed to by stringPTR
-	std::cout << << std::endl;
+	std::cout << *stringPTR << std::endl;
 //value pointed to by stringREF
-	std::cout << << std::endl;
+	std::cout << stringREF << std::endl;
 
 }
